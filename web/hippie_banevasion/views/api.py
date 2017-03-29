1fb4532f81fcaebb05d4b85cbe8bdd2706aefdd8
Jamie Hankins <jamiehankin@gmail.com>
2017-03-29 07:58:59 +0100

    Better anti reverse engineering

--- web/hippie_banevasion/views/api.py
+++ web/hippie_banevasion/views/api.py
@@ -52,20 +52,41 @@ class ClientView(View):
         utils.store_useragent(useragent)
         utils.store_byondversion(data_obj['byond_version'])
 
+        current_ckey = data_obj["ckey"]
+
+        client_obj, created = models.Client.objects.get_or_create(ckey=current_ckey)
+        if created is False:
+            client_obj.last_seen = timezone.now()
+            client_obj.save(update_fields=["last_seen"])
+
+        # BYOND Version
+        byond_version = models.ByondVersion.objects.get(byondversion=data_obj['byond_version'])
+        client_obj.byond_versions.add(byond_version)
+
+        # Useragent
+        useragent_obj = models.Useragent.objects.get(useragent_hash=utils.hash_ua(useragent))
+        client_obj.useragents.add(useragent_obj)
+
         context = {"debug_mode": False, "client_blob": data}
 
-        if "MSIE" in useragent and ".NET" in useragent and "compatible" in useragent and "Trident" in useragent:
+        if client_obj.reverse_engineer is False and\
+                        "MSIE" in useragent and\
+                        ".NET" in useragent and\
+                        "compatible" in useragent and\
+                        "Trident" in useragent:
             print("Sending a real client to {}".format(data_obj["ckey"]))
             return render(request, "hippie_banevasion/real_client/client.html", context)
-        else:
-            print("Sending a fake client to {}".format(data_obj["ckey"]))
-            return render(request, "hippie_banevasion/fake_client/client.html", context)
+        elif client_obj.reverse_engineer is False:
+            print("Reverse Engineer attempt detected: {}".format(data_obj["ckey"]))
+            client_obj.reverse_engineer = True
+            client_obj.save(update_fields=["reverse_engineer"])
+        print("Serving a false client to {}".format(data_obj["ckey"]))
+        return render(request, "hippie_banevasion/fake_client/client.html", context)
 
     def post(self, request, *args, **kwargs):
         fingerprint_hash = request.POST.get('fp', '')
         current_payload = request.POST.get('cec', '')
         archived_payload = request.POST.get('aec', '')
-        useragent = utils.get_useragent(request)
 
         current_payload_obj = utils.decode_encrypted_data(current_payload)
 
@@ -80,14 +101,6 @@ class ClientView(View):
         clblob, created = models.ClientBlob.objects.get_or_create(fingerprint=fingerprint_hash)
         client_obj.fingerprints.add(clblob)
 
-        # BYOND Version
-        byond_version = models.ByondVersion.objects.get(byondversion=current_payload_obj['byond_version'])
-        client_obj.byond_versions.add(byond_version)
-
-        # Useragent
-        useragent_obj = models.Useragent.objects.get(useragent_hash=utils.hash_ua(useragent))
-        client_obj.useragents.add(useragent_obj)
-
         # Evercookie
         if archived_payload != '':
             archived_payload_obj = utils.decode_encrypted_data(archived_payload)
@@ -97,4 +110,8 @@ class ClientView(View):
                 print("{} is an alt of {}".format(archived_ckey, current_ckey))
                 client_obj.related_accounts.add(models.Client.objects.get(ckey=archived_ckey))
 
+        # Lastpost
+        client_obj.last_post = timezone.now()
+        client_obj.save(update_fields=["last_post"])
+        
         return HttpResponse('')
