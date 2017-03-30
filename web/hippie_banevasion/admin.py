6e6842eabea0d01565417177f0c1f5b672287e9a
Jamie Hankins <jamiehankin@gmail.com>
2017-03-30 06:14:58 +0100

    Expose the admin functionality

--- web/hippie_banevasion/admin.py
+++ web/hippie_banevasion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from hippie_banevasion import models
 # Register your models here.
+
+admin.site.register(models.Client)
+admin.site.register(models.ClientBlob)
+admin.site.register(models.Useragent)
+admin.site.register(models.ByondVersion)
